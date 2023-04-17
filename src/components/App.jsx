9e96d1cb3da34b9d048a5ad893adc246b9b082de
michudud <pzlc1236@gmail.com>
2023-04-18 01:09:42 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+};
+
+const rootDiv = document.getElementById("root");
+if (rootDiv) {
+  createRoot(rootDiv).render(<App />);
+}
