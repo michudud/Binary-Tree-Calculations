47838bae95b23309740ebceefb69f57f822e1ff9
michudud <pzlc1236@gmail.com>
2023-04-18 01:37:14 +0200

    add example data structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import LeafCounter from "./LeafCounter";
 import EdgeCounter from "./EdgeCounter";
 import EquivalentChecker from "./EquivalentChecker";
+import { exampleStructure } from "./data_structures/example_structure";
 
 const App = () => {
   return (
     <div className="App">
-      <LeafCounter/>
-      <EdgeCounter/>
-      <EquivalentChecker/>
+      <LeafCounter structure={exampleStructure}/>
+      <EdgeCounter structure={exampleStructure}/>
+      <EquivalentChecker structure={exampleStructure}/>
     </div>
   );
 };
---
+++ src/components/data_structures/example_structure.js
@@ -0,0 +1,42 @@
+export const exampleStructure = [
+    {
+      value: 5,
+      children: [
+        {
+          value: 3,
+          children: [
+            {
+              value: 2,
+            },
+            {
+              value: 5,
+            },
+          ],
+        },
+        {
+          value: 7,
+          children: [
+            {
+              value: 1,
+            },
+            {
+              value: 0,
+              children: [
+                {
+                  value: 2,
+                },
+                {
+                  value: 8,
+                  children: [
+                    {
+                      value: 5,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
