611d55a4a71e7db2465d338dcccbcf0fc7c13564
michudud <pzlc1236@gmail.com>
2023-04-18 10:23:37 +0200

    handle text area change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import LeafCounter from "./LeafCounter";
 import EdgeCounter from "./EdgeCounter";
@@ -6,11 +6,23 @@ import EquivalentChecker from "./EquivalentChecker";
 import { exampleStructure } from "./data_structures/example_structure";
 
 const App = () => {
+  const [currDataStructure, setCurrDataStructure] = useState(exampleStructure);
+  const textAreaRef = useRef();
+  const handleClick = () => {
+    const structure = JSON.parse(textAreaRef.current.value);
+    setCurrDataStructure(structure);
+  };
+
   return (
     <div className="App">
-      <LeafCounter dataStructure={exampleStructure} />
-      <EdgeCounter dataStructure={exampleStructure} />
-      <EquivalentChecker dataStructure={exampleStructure} />
+      <LeafCounter dataStructure={currDataStructure} />
+      <EdgeCounter dataStructure={currDataStructure} />
+      <EquivalentChecker dataStructure={currDataStructure} />
+      <button onClick={handleClick}>Check Structure</button>
+      <textarea
+        ref={textAreaRef}
+        defaultValue={JSON.stringify(currDataStructure)}
+      ></textarea>
     </div>
   );
 };
